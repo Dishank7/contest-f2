73c349d2d6bd722b2240986c0089ec8baf6093be
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2023-05-14 00:05:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", marks: 80 },
-  { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
-];
+    { id: 1, name: "john", age: "18", marks: 80 },
+    { id: 2, name: "jack", age: "20", marks: 85 },
+    { id: 3, name: "karen", age: "19", marks: 35 },
+  ];
+  
+  function PrintStudentswithMap() {
+    //Write your code here , just console.log
+    let filteredArr = arr.map((student) => {
+      if (student.marks > 50) {
+        return student;
+      }
+    });
 
-function PrintStudentswithMap() {
-  //Write your code here , just console.log
-}
+    console.log(filteredArr);
+  }
+  
+  function PrintStudentsbyForEach() {
+    //Write your code here , just console.log
+    let filteredArr1 = [];
 
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-}
+arr.forEach((student) => {
+  if (student.marks > 50) {
+    filteredArr1.push(student);
+  }
+});
 
-function addData() {
-  //Write your code here, just console.log
-}
+console.log(filteredArr1);
+  }
+  
+  function addData() {
+    //Write your code here, just console.log
+    
+arr.push({id:4, name:"susan", age:"20", marks:45});
 
-function removeFailedStudent() {
-  //Write your code here, just console.log
-}
+console.log(arr);
+  }
+  
+  function removeFailedStudent() {
+    //Write your code here, just console.log
+    
+let filteredArr2 = arr.filter((student) => {
+  return student.marks >= 50;
+});
 
-function concatenateArray() {
-  //Write your code here, just console.log
-}
+console.log(filteredArr2);
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+  }
